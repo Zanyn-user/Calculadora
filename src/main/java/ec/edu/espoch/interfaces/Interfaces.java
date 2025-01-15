b54f4da510d1bfb37f98d3585d2233ede017f189
Zanyn-user <zanynuser@gmail.com>
2025-01-15 14:49:47 -0500

    primer comentario

---
+++ src/main/java/ec/edu/espoch/interfaces/Interfaces.java
@@ -0,0 +1,15 @@
+
+
+package ec.edu.espoch.interfaces;
+
+/**
+ *
+ * @author Eddy
+ */
+public class Interfaces {
+
+    public static void main(String[] args) {
+       Interfaz1 objInterfaz1 = new Interfaz1();
+       objInterfaz1.setVisible(true);
+    }
+}
